578814fe2e3aaf1434e82628d27cec6397d98d81
Harsh Bhatt <harshb669@gmail.com>
2021-04-23 19:10:50 +0530

    minor css changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,24 +4,22 @@ import requests from "utils/requests";
 function Navbar() {
   const router = useRouter();
   const currentGenre = router.query.genre;
-  const listOfCategory = Object.entries(requests)?.map(
-    ([key, { title, url }]) => {
-      const isCurrent = currentGenre === key;
-      return (
-        <h2
-          key={key}
-          onClick={() => {
-            router.push(`/?genre=${key}`);
-          }}
-          className={`last:pr-32 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-gray-500 ${
-            isCurrent && "text-bold text-white tracking-wider scale-125"
-          }`}
-        >
-          {title}
-        </h2>
-      );
-    }
-  );
+  const listOfCategory = Object.entries(requests)?.map(([key, { title }]) => {
+    const isCurrent = currentGenre === key;
+    return (
+      <h2
+        key={key}
+        onClick={() => {
+          router.push(`/?genre=${key}`);
+        }}
+        className={`last:pr-16 sm:last:pr-32 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-gray-500 ${
+          isCurrent && "text-bold text-white tracking-wider scale-125"
+        }`}
+      >
+        {title}
+      </h2>
+    );
+  });
   return (
     <nav className="relative">
       <div className="flex px-10 sm:px-20 text-lg whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
