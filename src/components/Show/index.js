103fa05882e764c39c5db815e71d72f5d0ff57fa
Harsh Bhatt <harshb669@gmail.com>
2021-04-25 11:35:25 +0530

    minor changes

--- src/components/Show/index.js
+++ src/components/Show/index.js
@@ -15,6 +15,27 @@ const SelectedShow = forwardRef(({ result, reviews }, ref) => {
         <h2 className="my-1 text-xl text-white tracking-wider font-bold">
           {result.title || result.original_name}
         </h2>
+        <div className="pr-2 py-1 flex items-center">
+          <StarIcon className="h-5" />
+          <span className="ml-1 text-sm font-semibold tracking-wider">
+            {result.vote_average}/10
+          </span>
+        </div>
+        {result.genres?.length && (
+          <div className="pr-2 flex flex-col sm:flex-row justify-center sm:items-center sm:justify-start">
+            <h3 className="font-bold tracking-widest">Genre</h3>
+            <div className="mt-2 sm:mt-0 sm:ml-2 text-white text-sm whitespace-nowrap space-x-2 sm:space-x-4">
+              {result.genres.map((genre) => (
+                <span
+                  key={genre.id}
+                  className="text-xs font-semibold tracking-wider transition ease-out rounded border-2 border-solid border-gray-400 text-gray-400 hover:text-black hover:bg-white hover:border-white px-1 py-0.5"
+                >
+                  {genre.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
         <p className="text-sm text-justify py-3 pr-4">{result.overview}</p>
         <p className="flex items-center uppercase text-sm">
           {result.media_type && `${result.media_type} •`}{" "}
@@ -22,27 +43,6 @@ const SelectedShow = forwardRef(({ result, reviews }, ref) => {
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
         </p>
       </div>
-      <div className="px-2 flex items-center">
-        <StarIcon className="h-5" />
-        <span className="ml-1 text-sm font-semibold tracking-wider">
-          {result.vote_average}/10
-        </span>
-      </div>
-      {result.genres?.length && (
-        <div className="p-2 flex flex-col sm:flex-row justify-center sm:items-center sm:justify-start">
-          <h3 className="font-bold tracking-widest">Genre</h3>
-          <div className="mt-2 sm:mt-0 sm:ml-2 text-white text-sm whitespace-nowrap space-x-2 sm:space-x-4">
-            {result.genres.map((genre) => (
-              <span
-                key={genre.id}
-                className="text-xs font-semibold tracking-wider transition ease-out rounded border-2 border-solid border-gray-400 text-gray-400 hover:text-black hover:bg-white hover:border-white px-1 py-0.5"
-              >
-                {genre.name}
-              </span>
-            ))}
-          </div>
-        </div>
-      )}
     </div>
   );
 });
